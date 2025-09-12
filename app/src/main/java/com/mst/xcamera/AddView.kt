186c48e986c6c262ae792b9a05a4f06f23c9c746
mauriciofigueredo <mauriciofigueredo46@gmail.com>
2025-09-11 22:47:47 -0300

    Initial commit

---
+++ app/src/main/java/com/mst/xcamera/AddView.kt
@@ -0,0 +1,129 @@
+package com.mst.xcamera
+
+import android.Manifest
+import android.content.ContentValues
+import android.content.Context
+import android.os.Build
+import android.os.Environment
+import android.view.ViewGroup
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import java.io.File
+import java.util.concurrent.Executor
+
+import android.provider.MediaStore
+
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun AddView() {
+    val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+
+    val context = LocalContext.current
+    val cameraController = remember {
+        LifecycleCameraController(context)
+    }
+    val lifecycle = LocalLifecycleOwner.current
+
+    LaunchedEffect(Unit) {
+        permissionState.launchPermissionRequest()
+    }
+    Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
+        FloatingActionButton(onClick = {
+            val executor = ContextCompat.getMainExecutor(context)
+            takePicture(cameraController, executor, context)
+        }) {
+            Icon(imageVector = Icons.Default.AddCircle, contentDescription = "Take photo", modifier = Modifier.size(35.dp))
+        }
+    }) {
+        if (permissionState.status.isGranted) {
+            CamaraComposable(cameraController, lifecycle, modifier = Modifier.padding(it))
+        } else {
+            Text(text = "Permiso Denegado!", modifier = Modifier.padding(it))
+        }
+    }
+}
+
+
+//------------------*********  TAKE PHOTO  ***********--------------------
+private fun takePicture(cameraController: LifecycleCameraController, executor: Executor, context: Context) {
+    //val file = File.createTempFile("imagentest", ".jpg")
+    //val outputDirectory = ImageCapture.OutputFileOptions.Builder(file).build()
+
+
+    val name = "miFoto-V"
+    val albumName = "MiAppCameraX" // Nombre del álbum en la galería
+
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+        put(MediaStore.MediaColumns._ID, name)
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM + "/$albumName")
+        }
+    }
+
+    val outputOptions = ImageCapture.OutputFileOptions.Builder(
+        context.contentResolver,
+        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        contentValues
+    ).build()
+
+    cameraController.takePicture(
+        outputOptions,
+        executor,
+        object : ImageCapture.OnImageSavedCallback {
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                println(outputFileResults.savedUri)
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                println()
+            }
+        },
+    )
+}
+
+@Composable
+fun CamaraComposable(
+    cameraController: LifecycleCameraController,
+    lifecycle: LifecycleOwner,
+    modifier: Modifier = Modifier,
+) {
+    cameraController.bindToLifecycle(lifecycle)
+    AndroidView(modifier = modifier, factory = { context ->
+        val previewView = PreviewView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+            )
+        }
+        previewView.controller = cameraController
+
+        previewView
+    })
+}
